fa86d1a4c7d686e2f7b73dc8ab48c62a1d6c3cf3
nhkim91 <nhkim91@hotmail.com>
2014-12-01 14:54:32 +0100

    level

--- src/level.cpp
+++ src/level.cpp
@@ -1,5 +1,17 @@
 #include "level.h"
+#include "alien.h"
+#include <vector>
 
-level::level()
+using namespace std;
+
+void level::spawn()
 {
+    int i;
+    i = rand();
+    position[1] = rand()/600
+
+    if(i < 100)
+    {
+        alien_mk1 alien_1(1,position)
+    }
 }
--- src/level.h
+++ src/level.h
@@ -1,10 +1,42 @@
 #ifndef LEVEL_H
 #define LEVEL_H
+#include <vector>
+
 
 class level
 {
 public:
-    level();
+    level() = default;
+    ~level() = default;
+
+    level(const level& l);
+
+    virtual void spawn();
+
+
+private:
+
+    std::vector<int>position(2);
+    position[0] = 100; //Startposition i bredd
 };
 
+class level_1 : public level
+{
+public:
+    level_1() = default;
+    ~level_1();
+
+};
+
+
+class level_2 : public level
+{
+public:
+    level_2() = default;
+    ~level_2();
+
+};
+
+
 #endif // LEVEL_H
+
