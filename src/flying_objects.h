12d1a6af1827d3d6d9a002ef8eaae5dce999cfa4
madeleineardic <madar730@student.liu.se>
2014-12-03 16:54:13 +0100

    Mags SDL_image borta

--- src/flying_objects.h
+++ src/flying_objects.h
@@ -9,12 +9,12 @@
 #define FLYING_OBJECTS_H_
 #include <vector>
 #include <SDL2/SDL.h>
-//#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_image.h>
 
-#undef main
+//#undef main
 
 //#include <SDL2/SDL_image.h>
-#include <SDL2_image/SDL_image.h> //<- maggie behöver denna!
+//#include <SDL2_image/SDL_image.h> //<- maggie behöver denna!
 
 
 
