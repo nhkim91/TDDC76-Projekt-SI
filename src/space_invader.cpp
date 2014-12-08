0edd8794c00f755d4671d0c6fdf3705c87c9880f
madeleineardic <madar730@student.liu.se>
2014-12-08 17:35:17 +0100

    lagt över main loop till space_invader

--- src/space_invader.cpp
+++ src/space_invader.cpp
@@ -1,16 +1,12 @@
 #include "space_invader.h"
-#include "linkheader.h"
 #include <iostream>
 #include <algorithm>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
-
 using namespace std;
 
-space_invader::space_invader()
-{
 
-}
+
 
 void space_invader::killer()
 {
@@ -56,12 +52,11 @@ bool space_invader::collides(const flying_objects& obj_1, const flying_objects& 
 			(a.x + a.w  >  b.x  &&  a.x + a.w  <  b.x + b.w)
 	)
 	{
-
 		if (
 				// is a.y inside b's y-range?
-				(a.y        >  b.y  &&  a.y        <  b.y + b.h) ||
+				(a.y > b.y  &&  a.y < b.y + b.h) ||
 				// is a.y's other corner inside b's y-range?
-				(a.y + a.h  >  b.y  &&  a.y + a.h  <  b.y + b.h)
+				(a.y + a.h > b.y  &&  a.y + a.h < b.y + b.h)
 		)
 		{
 			return true;
@@ -70,4 +65,101 @@ bool space_invader::collides(const flying_objects& obj_1, const flying_objects& 
 	return false;
 }
 
-//bool erase
+void space_invader::run()
+{
+	// make the scaled rendering look smoother
+	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
+	// render at a virtual resolution then stretch to actual resolution
+	SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
+	// load figures
+
+	SDL_Texture* player{nullptr};
+	SDL_Texture* alien{nullptr};
+	int ballWidth{0};
+	int ballHeight{0};
+	{
+		SDL_Surface* temp = IMG_Load("player.png");
+
+		player = SDL_CreateTextureFromSurface(renderer, temp);
+		ballWidth = temp->w;
+		ballHeight = temp->h;
+
+		SDL_FreeSurface(temp);
+
+		/*temp = IMG_Load("alien.png");
+		alien = SDL_CreateTextureFromSurface(renderer, temp);
+
+		SDL_FreeSurface(temp);*/
+	}
+	// set ball position
+	SDL_Rect ballRect;
+	ballRect.x = 10;
+	ballRect.y = 10;
+	ballRect.w = ballWidth;
+	ballRect.h = ballHeight;
+	//int xSpeed{0};
+	int ySpeed{0};
+	// main loop
+	bool running{true};
+	while (running) {
+		// handle events
+		SDL_Event event;
+		while (SDL_PollEvent(&event)) {
+
+			if (event.type == SDL_QUIT) {
+				running = false;
+			}
+			else if (event.type == SDL_KEYDOWN) {
+				if (event.key.keysym.sym == SDLK_ESCAPE) {
+					running = false;
+				}
+				else if (event.key.keysym.sym == SDLK_UP) {
+					ySpeed = -10;
+					cerr << "upp";
+				}
+				else if (event.key.keysym.sym == SDLK_DOWN) {
+					ySpeed = 10;
+				}
+			}
+			else if(event.type == SDL_KEYUP){
+				if (event.key.keysym.sym == SDLK_UP)
+				{
+					ySpeed = 0;
+				}
+				else if (event.key.keysym.sym == SDLK_DOWN)
+				{
+					ySpeed = 0;
+				}
+			}
+		}
+
+		// update things
+		ballRect.y += ySpeed;
+
+		if (ballRect.y + ballHeight > SCREEN_HEIGHT || ballRect.y < 0) {
+			ySpeed = -ySpeed;
+		}
+
+		// clear screen
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+		SDL_RenderClear(renderer);
+
+		// draw things
+		SDL_RenderCopy(renderer, player, nullptr, &ballRect);
+
+		// show the newly drawn things
+		SDL_RenderPresent(renderer);
+
+		// wait before drawing the next frame
+		SDL_Delay(10);
+	}
+	// free memory
+	SDL_DestroyTexture(player);
+	player = nullptr;
+	SDL_DestroyTexture(alien);
+	alien = nullptr;
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+
+	SDL_Quit();
+}
--- src/space_invader.h
+++ src/space_invader.h
@@ -2,17 +2,24 @@
 #define SPACE_INVADER_H
 #include <vector>
 #include "flying_objects.h"
-
 class space_invader
 {
 public:
-    space_invader();
+
+	const int SCREEN_WIDTH = 800;
+	const int SCREEN_HEIGHT = 600;
+
+	// create the window
+	SDL_Window* window = SDL_CreateWindow("SDL Testet", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_RESIZABLE);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+
+    space_invader() = default;
     std::vector<flying_objects> displaying_objects_;
 
     void killer();
     bool my_sort(int i, int j);
     bool collides(const flying_objects&, const flying_objects&);
-
+    void run();
 };
 
 #endif // SPACE_INVADER_H
