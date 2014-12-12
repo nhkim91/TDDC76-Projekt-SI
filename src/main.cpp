32979271f3f18f9252552b8376dfc60426ae48c0
nhkim91 <nhkim91@hotmail.com>
2014-12-12 17:54:59 +0100

    Tagit bort blue,green,red och flying_objects_test.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -33,13 +33,25 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-
-
     SDL_Window* window = SDL_CreateWindow("Highscore", SDL_WINDOWPOS_UNDEFINED,
                                           SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED |
                              SDL_RENDERER_PRESENTVSYNC);
 
+    SDL_Surface* temp = IMG_Load("space.png");
+
+    SDL_Texture* texture_ = SDL_CreateTextureFromSurface(renderer, temp);
+
+    SDL_Rect rect_;
+    rect_.w = temp->w;
+    rect_.h = temp->h;
+
+    SDL_FreeSurface(temp);
+    SDL_RenderPresent(temp);
+    SDL_RenderClear(renderer);
+    SDL_Delay(2000);
+
+/*
     render rend;
     rend.set_renderer(renderer);
 
@@ -48,7 +60,7 @@ int main(int argc, char *argv[])
     hs.save_score(10000);
     hs.show_highscore();
 
-    SDL_Delay(2000);
+    SDL_Delay(2000);*/
 
     //space_invader SI;
     //SI.run();
