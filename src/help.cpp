10d99a1cbcc4c04dbce29fde1f656e9552191059
kerso255 <me.kerstin@live.se>
2014-12-13 13:57:18 +0100

    help-beskrivning

--- src/help.cpp
+++ src/help.cpp
@@ -8,17 +8,23 @@ void help::set_renderer(render* rend)
 
 void help::show_help()
 {
-    renderer->render_image("space_background.png",0, 0);
+    renderer->render_image("Space.png",0, 0);
 
     SDL_Color whiteColor {255, 255, 255, 255};
     SDL_Color redColor {187, 32, 26, 255};
 
-    std::string msg = "Pil upp och ner styr rymdskeppet.";
-    std::string msg1 = "Använd space för att skjuta fienden.";
+    std::string msg = "Arrow up and down navigates the spaceship.";
+    std::string msg1 = "Use space to shoot the enemy.";
+    std::string msg2 = "Try not to be hit by the meteorites";
+    std::string msg3 = "or when the aliens shoot you. And don't";
+    std::string msg4 = "let the aliens over-run your planet.";
 
     renderer->render_text("Help", "Arcade.ttf", whiteColor, 100, 300, 30);
     renderer->render_text(msg, "Arcade.ttf", whiteColor, 30, 80, 200);
     renderer->render_text(msg1, "Arcade.ttf", whiteColor, 30, 80, 250);
+    renderer->render_text(msg2, "Arcade.ttf", whiteColor, 30, 80, 300);
+    renderer->render_text(msg3, "Arcade.ttf", whiteColor, 30, 80, 350);
+    renderer->render_text(msg4, "Arcade.ttf", whiteColor, 30, 80, 400);
     renderer->render_text("Back to Menu", "Arcade.ttf", redColor, 40, 280, 550);
 
     renderer->present();
