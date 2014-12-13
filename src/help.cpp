c4a58a2e8bdcf115c0c7aa7f844877dba0475a3a
kerso255 <me.kerstin@live.se>
2014-12-13 11:59:27 +0100

    Quit-knapp, Menu-knappar

--- src/help.cpp
+++ src/help.cpp
@@ -11,6 +11,7 @@ void help::show_help()
     renderer->render_image("space_background.png",0, 0);
 
     SDL_Color whiteColor {255, 255, 255, 255};
+    SDL_Color redColor {187, 32, 26, 255};
 
     std::string msg = "Pil upp och ner styr rymdskeppet.";
     std::string msg1 = "Använd space för att skjuta fienden.";
@@ -18,6 +19,7 @@ void help::show_help()
     renderer->render_text("Help", "Arcade.ttf", whiteColor, 100, 300, 30);
     renderer->render_text(msg, "Arcade.ttf", whiteColor, 30, 80, 200);
     renderer->render_text(msg1, "Arcade.ttf", whiteColor, 30, 80, 250);
+    renderer->render_text("Back to Menu", "Arcade.ttf", redColor, 40, 280, 550);
 
     renderer->present();
 
@@ -30,7 +32,7 @@ void help::show_help()
         if (event.type == SDL_KEYDOWN)
         {
             SDL_Keycode key = event.key.keysym.sym;
-            if (key == SDLK_ESCAPE)
+            if (key == SDLK_ESCAPE || key == SDLK_RETURN)
             {
                 run = false;
                 break;
