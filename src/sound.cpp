ae846faf1c87adef22413652a9dbd562c7e059d6
Sieuwa <marvi154@student.liu.se>
2014-12-16 19:40:58 +0100

    bytt ljud på power_up attack skott

--- src/sound.cpp
+++ src/sound.cpp
@@ -30,12 +30,20 @@ sound::sound()
 		fprintf(stderr, "unable to load attack sound %s\n", Mix_GetError());
 	}
 
-	attack_up_sound = Mix_LoadWAV("Laser_blast.waw");
+	attack_up_sound = Mix_LoadWAV("Laser_blast.ogg");
 	if (attack_sound == NULL)
 	{
 		fprintf(stderr, "unable to load attack_up sound %s\n", Mix_GetError());
 	}
 
+
+	/*kill_sound = Mix_LoadWAV("");
+	if (kill_sound == NULL)
+	{
+		fprintf(stderr, "unable to load kill sound %s\n", Mix_GetError());
+	}
+
+	*/
 	background_sound = Mix_LoadMUS("background.ogg");
 	if (background_sound == NULL)
 	{
@@ -52,6 +60,14 @@ sound::sound()
 void sound::play_attack()
 {
 	if ((Mix_PlayChannel(-1, attack_sound, 0)) == -1)
+	{
+		fprintf(stderr, "unable to play attack sound: %s\n", Mix_GetError());
+	}
+}
+
+void sound::play_attack_up()
+{
+	if ((Mix_PlayChannel(-1, attack_up_sound, 0)) == -1)
 	{
 		fprintf(stderr, "unable to play attack sound: %s\n", Mix_GetError());
 	}
