b9da7f4fbd2b0b69694ea642d82e546962853d6c
Sieuwa <marvi154@student.liu.se>
2014-12-16 16:43:53 +0100

    håller på med sound

--- src/main.cpp
+++ src/main.cpp
@@ -17,76 +17,77 @@ using namespace std;
 template <typename T>
 std::string to_string(T value)
 {
-    //create an output string stream
-    std::ostringstream os ;
+	//create an output string stream
+	std::ostringstream os ;
 
-    //throw the value into the string stream
-    os << value ;
+	//throw the value into the string stream
+	os << value ;
 
-    //convert the string stream into a string and return
-    return os.str() ;
+	//convert the string stream into a string and return
+	return os.str() ;
 }
 
 int main(int, char**)
 {
-    // initialize SDL
-    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0)
-    {
-        cerr << "Error initializing SDL" << endl;
-        exit(1);
-    }
-    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2 , 2048) < 0)
-    {
-        printf("SDL_mixer could not initialize! SDL_mixer Error: %s\n", Mix_GetError());
-    }
-    Mix_AllocateChannels(16);
+	// initialize SDL
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0)
+	{
+		cerr << "Error initializing SDL" << endl;
+		exit(1);
+	}
+	if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2 , 2048) < 0)
+	{
+		printf("SDL_mixer could not initialize! SDL_mixer Error: %s\n", Mix_GetError());
+	}
+	Mix_AllocateChannels(16);
 
 
-    SDL_Window* window = SDL_CreateWindow("Highscore", SDL_WINDOWPOS_UNDEFINED,
-                                          SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
+	SDL_Window* window = SDL_CreateWindow("Highscore", SDL_WINDOWPOS_UNDEFINED,
+			SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
 
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED |
-                             SDL_RENDERER_PRESENTVSYNC);
-
-
-    sound all_sounds = sound();
-    render rend;
-    rend.set_renderer(renderer);
-
-    space_invader SI(renderer, &rend, &all_sounds);
-
-    //space_invader SI(renderer, &rend);
-
-    //SI.run(); // TODO: Bra om space_invader retunerar score.
-
-    highscore hs;
-    hs.set_renderer(&rend);
-
-    help h;
-    h.set_renderer(&rend);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED |
+			SDL_RENDERER_PRESENTVSYNC);
 
 
+	sound all_sounds = sound();
+	render rend;
+	rend.set_renderer(renderer);
 
+	all_sounds.play_menu();
+	space_invader SI(renderer, &rend, &all_sounds);
 
+	//space_invader SI(renderer, &rend);
 
-    menu m;
-    m.set_renderer(&rend);
-    m.set_highscore(&hs);
-    m.set_help(&h);
-    m.set_sound(&all_sounds);
-    //m.set_play(&SI);
-    m.run();
+	//SI.run(); // TODO: Bra om space_invader retunerar score.
+
+	highscore hs;
+	hs.set_renderer(&rend);
+
+	help h;
+	h.set_renderer(&rend);
 
 
 
-    // free memory
 
 
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
+	menu m;
+	m.set_renderer(&rend);
+	m.set_highscore(&hs);
+	m.set_help(&h);
+	m.set_sound(&all_sounds);
+	//m.set_play(&SI);
+	m.run();
 
-    SDL_Quit();
 
-    return 0;
+
+	// free memory
+
+
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+
+	SDL_Quit();
+
+	return 0;
 }
 
--- src/sound.cpp
+++ src/sound.cpp
@@ -12,56 +12,72 @@ using namespace std;
 
 sound::~sound()
 {
-    Mix_FreeChunk(attack_sound);
+	Mix_FreeChunk(attack_sound);
 }
 
 sound::sound()
 {
-    attack_sound = Mix_LoadWAV("sfx_laser1.ogg");
-    if (attack_sound == NULL)
-    {
-        fprintf(stderr, "unable to load attack sound %s\n", Mix_GetError());
-    }
+	attack_sound = Mix_LoadWAV("sfx_laser1.ogg");
+	if (attack_sound == NULL)
+	{
+		fprintf(stderr, "unable to load attack sound %s\n", Mix_GetError());
+	}
 
-    attack_up_sound = Mix_LoadWAV("Laser_blast.waw");
-       if (attack_sound == NULL)
-       {
-           fprintf(stderr, "unable to load attack_up sound %s\n", Mix_GetError());
-       }
+	attack_up_sound = Mix_LoadWAV("Laser_blast.waw");
+	if (attack_sound == NULL)
+	{
+		fprintf(stderr, "unable to load attack_up sound %s\n", Mix_GetError());
+	}
 
-    background_sound = Mix_LoadMUS("background.ogg");
-    if (background_sound == NULL)
-    {
-        fprintf(stderr, "unable to load background sound  %s\n", Mix_GetError());
-    }
-    //menu_sound = Mix_LoadMUS("menu_sound.ogg");
-    //if (menu_sound == NULL)
-    //{
-      //  fprintf(stderr, "unable to load menu sound %s\n", Mix_GetError());
-    //}
+	background_sound = Mix_LoadMUS("background.ogg");
+	if (background_sound == NULL)
+	{
+		fprintf(stderr, "unable to load background sound  %s\n", Mix_GetError());
+	}
+	menu_sound = Mix_LoadMUS("menu_sound.ogg");
+	if (menu_sound == NULL)
+	{
+		fprintf(stderr, "unable to load menu sound %s\n", Mix_GetError());
+	}
+
 }
 
 void sound::play_attack()
 {
-    if ((Mix_PlayChannel(-1, attack_sound, 0)) == -1)
-    {
-        fprintf(stderr, "unable to play attack sound: %s\n", Mix_GetError());
-    }
+	if ((Mix_PlayChannel(-1, attack_sound, 0)) == -1)
+	{
+		fprintf(stderr, "unable to play attack sound: %s\n", Mix_GetError());
+	}
 }
 
 void sound::play_background()
 {
-    if (Mix_PlayingMusic() == 0)
-    {
-        Mix_PlayMusic(background_sound, -1);
-    }
+	if (Mix_PlayingMusic() == 0)
+	{
+		Mix_PlayMusic(background_sound, -1);
+	}
 }
-
 
 void sound::play_menu()
 {
-    if (Mix_PlayingMusic() == 0)
-    {
-        Mix_PlayMusic(menu_sound, -1);
-    }
+	if (Mix_PlayingMusic() == 0)
+	{
+		Mix_PlayMusic(menu_sound, -1);
+	}
+}
+
+void sound_paused()
+{
+	//If the music is paused
+	if( Mix_PausedMusic() == 1 )
+	{
+		//Resume the music
+		Mix_ResumeMusic();
+	}
+	//If the music is playing
+	else
+	{
+		//Pause the music
+		Mix_PauseMusic();
+	}
 }
